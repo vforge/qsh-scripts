45b2422a18711f3e3f76e6819d2df02c7d5a7f6f
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2019-11-01 22:14:00 -0700

    sigh user

--- get-ids.sh
+++ get-ids.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-export DOCKERUID=`id -g dockeruser`
+export DOCKERUID=`id -u dockeruser`
 export DOCKERGID=`getent group docker | sed 's/docker:x://' | sed 's/:.*//'`
 
-export SAMBAUID=`id -g sambauser`
-export SAMBAGID=`getent group sambashare | sed 's/sambashare:x://' | sed 's/:.*//'`
+# Just Use dockeruser:docker
+# export SAMBAUID=`id -u sambauser`
+# export SAMBAGID=`getent group sambashare | sed 's/sambashare:x://' | sed 's/:.*//'`
 
 echo "> Docker: ${DOCKERUID}:${DOCKERGID}"
-echo "> Samba: ${SAMBAUID}:${SAMBAGID}"
+# echo "> Samba: ${SAMBAUID}:${SAMBAGID}"
